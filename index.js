ec6a1c2400d59ffb7c319b80fb5dd04fefd75bfc
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-08-28 12:22:58 -0300

    adicionando itens no carrinho

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botaoAdicionar = document.getElementById("adicionar-item");
-let contador = 0;
-
+const listaDeCompras = document.getElementById("lista-de-compras");
 const inputItem = document.getElementById("input-item");
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -16,5 +16,29 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";
     inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista)
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+        weekday:"long"
+    });
+
+    const data = new Date().toLocaleDateString("pt-BR")
+    const hora = new Date().toLocaleTimeString("pt-BR", {
+        hour: "numeric",
+        minute: "numeric"
+    })
+
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data")
+    itemDaLista.appendChild(itemData)
+
+    listaDeCompras.appendChild(itemDaLista) 
 });
- 
