19d1945d70366821b658ad71d381774a9753be33
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-08-27 21:08:54 -0300

    JS estudos

---
+++ index.js
@@ -0,0 +1,20 @@
+const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
+
+const inputItem = document.getElementById("input-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if(inputItem.value === ""){
+        alert("Por favor, insira um item!");
+        return
+    }
+    
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+});
+ 
